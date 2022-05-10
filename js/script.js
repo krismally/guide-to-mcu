4bbc9d6fb55f731298de2b68667a6ddc52435149
Kris Mally <kristophermally@gmail.com>
2022-05-09 19:45:36 -0600

    Add background gif and styling, initialize js file

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// VARIABLES
+const privKey = "88d62536fe485fc62ff2e3eb075a4cb9b14616ea"
+const pubKey = "6ec8fb5a2eeca354fabae121f1746475"
+const URL = ""
+
+
+
+// ELEMENT REFERENCES
+
+// EVENT LISTENERS
+$("document").ready(function(){
+    console.log("Success!");
+})
