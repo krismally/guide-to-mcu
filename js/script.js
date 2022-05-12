d0b4e4a4418e630f4651772321a76952681503a7
Kris Mally <kristophermally@gmail.com>
2022-05-12 13:59:30 -0600

    Finish random char generator functionality

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const URL = "https://www.superheroapi.com/api.php/338148107599656/search/";
 
 // ELEMENT REFERENCES
 const $getChar = $(".get-character");
+const $infGauntlet = $(".random-character")
 const $charCard = $(".char-card");
 const $img = $(".char-picture");
 const $close = $(".close");
@@ -16,6 +17,7 @@ const $input = $("input[type='text']");
 // EVENT LISTENERS
 $("document").ready(function(){
     $($charCard).hide();
+    $($infGauntlet).click(randomChar)
     $("form").on("submit", handleGetData);
     $($close).click(removeCard)
 })
@@ -26,7 +28,6 @@ function handleGetData(event) {
     userInput = $input.val()
     // get data from API based on user input
     $.ajax(URL + userInput).then((data) => {
-        console.log(data);
         render(data);
         card();
         },
@@ -35,7 +36,29 @@ function handleGetData(event) {
     });
 }
 
-
+function randomChar() {
+    const chars = [30, 489, 313, 346, 414, 655, 659, 470, 720, 226, 620, 566, 579, 587, 107, 149, 156, 379];
+    const randomChar = Math.floor(Math.random() * chars.length)
+    const allChar = `https://www.superheroapi.com/api.php/338148107599656/${chars[randomChar]}`
+    $.ajax(allChar).then((data) => {
+        console.log(data);
+        $img.attr("src", `${data.image.url}`)
+        $name.text(`Name: ${data.name}`)
+        $species.text(`Species: ${data.appearance.race}`)
+        $powers.text(`Power Stats: 
+        Intelligence - ${data.powerstats.intelligence},
+        Strength - ${data.powerstats.strength},
+        Speed - ${data.powerstats.speed},
+        Durability - ${data.powerstats.durability},
+        Power - ${data.powerstats.power},
+        Combat - ${data.powerstats.combat}`)
+        $relatives.text(`Relatives: ${data.connections.relatives}`)
+        card()
+        },
+        (error) => {
+            console.log("bad request", error);
+    });
+}
 
 function render(data) {
     if (data.results[0].id == 405) {
@@ -65,8 +88,6 @@ function render(data) {
     }
 }
 
-
-
 function card() {
     // hides the getChar box and shows the charCard box after user search
     $($getChar).hide("slow");
@@ -79,3 +100,4 @@ function removeCard() {
     $($charCard).hide("slow");
     $($getChar).show("slow");
 }
+
