d1dc37cda1bada73ec7d81b50ea99f919dcc2464
Kris Mally <kristophermally@gmail.com>
2022-05-13 06:44:20 -0600

    Clarify code with comments

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ const $input = $("input[type='text']");
 // EVENT LISTENERS
 $("document").ready(function(){
     $($charCard).hide();
-    $($infGauntlet).click(randomChar)
+    $($infGauntlet).click(randomChar);
     $("form").on("submit", handleGetData);
     $($close).click(removeCard);
 })
@@ -25,7 +25,7 @@ $("document").ready(function(){
 // FUNCTIONS
 function handleGetData(event) {
     event.preventDefault();
-    userInput = $input.val()
+    userInput = $input.val();
     // get data from API based on user input
     $.ajax(URL + userInput).then((data) => {
         render(data);
@@ -37,23 +37,25 @@ function handleGetData(event) {
 }
 
 function randomChar() {
+    // array storing marvel char id's
     const chars = [30, 489, 313, 346, 414, 655, 659, 470, 226, 620, 566, 579, 587, 107, 149, 157, 379, 714];
-    const randomChar = Math.floor(Math.random() * chars.length)
-    const allChar = `https://www.superheroapi.com/api.php/338148107599656/${chars[randomChar]}`
+    // randomize index selected and interpolate onto API link to display char card
+    const randomChar = Math.floor(Math.random() * chars.length);
+    const allChar = `https://www.superheroapi.com/api.php/338148107599656/${chars[randomChar]}`;
     $.ajax(allChar).then((data) => {
-        console.log(data);
-        $img.attr("src", `${data.image.url}`)
-        $name.text(`Name: ${data.name}`)
-        $species.text(`Species: ${data.appearance.race}`)
+        // add char card info
+        $img.attr("src", `${data.image.url}`);
+        $name.text(`Name: ${data.name}`);
+        $species.text(`Species: ${data.appearance.race}`);
         $powers.text(`Power Stats: 
         Intelligence - ${data.powerstats.intelligence},
         Strength - ${data.powerstats.strength},
         Speed - ${data.powerstats.speed},
         Durability - ${data.powerstats.durability},
         Power - ${data.powerstats.power},
-        Combat - ${data.powerstats.combat}`)
-        $relatives.text(`Relatives: ${data.connections.relatives}`)
-        card()
+        Combat - ${data.powerstats.combat}`);
+        $relatives.text(`Relatives: ${data.connections.relatives}`);
+        card();
         },
         (error) => {
             console.log("bad request", error);
@@ -61,30 +63,32 @@ function randomChar() {
 }
 
 function render(data) {
+    // if char id is incorrect character when searching thor, change result array element to correct thor
     if (data.results[0].id == 405) {
-        $img.attr("src", `${data.results[1].image.url}`)
-        $name.text(`Name: ${data.results[1].name}`)
-        $species.text(`Species: ${data.results[1].appearance.race}`)
+        $img.attr("src", `${data.results[1].image.url}`);
+        $name.text(`Name: ${data.results[1].name}`);
+        $species.text(`Species: ${data.results[1].appearance.race}`);
         $powers.text(`Power Stats: 
         Intelligence - ${data.results[1].powerstats.intelligence},
         Strength - ${data.results[1].powerstats.strength},
         Speed - ${data.results[1].powerstats.speed},
         Durability - ${data.results[1].powerstats.durability},
         Power - ${data.results[1].powerstats.power},
-        Combat - ${data.results[1].powerstats.combat}`)
-        $relatives.text(`Relatives: ${data.results[1].connections.relatives}`)
+        Combat - ${data.results[1].powerstats.combat}`);
+        $relatives.text(`Relatives: ${data.results[1].connections.relatives}`);
     } else {
-        $img.attr("src", `${data.results[0].image.url}`)
-        $name.text(`Name: ${data.results[0].name}`)
-        $species.text(`Species: ${data.results[0].appearance.race}`)
+        //add char card info
+        $img.attr("src", `${data.results[0].image.url}`);
+        $name.text(`Name: ${data.results[0].name}`);
+        $species.text(`Species: ${data.results[0].appearance.race}`);
         $powers.text(`Power Stats: 
         Intelligence - ${data.results[0].powerstats.intelligence},
         Strength - ${data.results[0].powerstats.strength},
         Speed - ${data.results[0].powerstats.speed},
         Durability - ${data.results[0].powerstats.durability},
         Power - ${data.results[0].powerstats.power},
-        Combat - ${data.results[0].powerstats.combat}`)
-        $relatives.text(`Relatives: ${data.results[0].connections.relatives}`)
+        Combat - ${data.results[0].powerstats.combat}`);
+        $relatives.text(`Relatives: ${data.results[0].connections.relatives}`);
     }
 }
 
